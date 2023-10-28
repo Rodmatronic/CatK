0cbc3be262d67c1bd3c4795d51dafeeffa498638
Rodmatronic <105672808+Rodmatronic@users.noreply.github.com>
2023-10-28 12:31:27 -0300

    Separate sleep function

---
+++ src/bin/sleep.c
@@ -0,0 +1,19 @@
+
+
+// Function to wait for a specified number of seconds
+void sleep(int seconds) {
+    uint8 start_time = read_RTC_register(RTC_SECONDS);
+
+    while (1) {
+        uint8 current_time = read_RTC_register(RTC_SECONDS);
+        if (current_time != start_time) {
+            if (current_time < start_time) {
+                current_time += 1; // Handle rollover from 59 to 00
+            }
+
+            if (current_time - start_time >= seconds) {
+                break;
+            }
+        }
+    }
+}
