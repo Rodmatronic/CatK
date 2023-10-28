ceb1043d4c66bf5630c456ff58e01e17b84ab329
Rodmatronic <105672808+Rodmatronic@users.noreply.github.com>
2023-10-27 22:07:55 -0300

    Defines bootmessages, wait_seconds and boot.

--- include/kernel.h
+++ include/kernel.h
@@ -3,6 +3,10 @@
 
 #include "types.h"
 
+void bootmessage(const char* str);
+void wait_seconds(int seconds);
+void boot();
+
 // symbols from linker.ld for section addresses
 extern uint8 __kernel_section_start;
 extern uint8 __kernel_section_end;
