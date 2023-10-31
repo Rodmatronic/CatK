52624d7591684d481d6430679712f90e74cf023c
Rodmatronic <105672808+Rodmatronic@users.noreply.github.com>
2023-10-31 08:15:37 -0300

    Changed ver to 0.04.1

--- src/PreBoot.c
+++ src/PreBoot.c
@@ -22,7 +22,7 @@ void PreBoot()
     printf("+==================================+\n");
 
     console_gotoxy(0, 24);
-    printf_dark("Build num 0.041");
+    printf_dark("Build num 0.04.1");
 
     console_gotoxy(40, 5);
     printf_blue("           __           __            Z ");
