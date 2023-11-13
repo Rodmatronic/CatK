f34f36de5fccaec3831d5403b34fe51b2f232a88
Rodmatronic <roderickmpike@icloud.com>
2023-11-13 00:00:37 -0400

    Version 0.05 is here!

--- include/config.h
+++ include/config.h
@@ -10,9 +10,9 @@ extern char* bootargs;
 char host_name[30] = ""; // hostname with a maximum length of 63 characters
 char username[30] = ""; // username
 
-uint32 versionnumber = "0.04.8";
+uint32 versionnumber = "0.05";
 char* vername = "CatK Version Boppin";
-uint32 prebootversion = "0.04.8";
+uint32 prebootversion = "0.05";
 char* bootargs = "n/a"; // Boot args
 
 #endif
