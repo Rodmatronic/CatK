4a72c5ed48d98a0fb607caca70d6076a13825632
Rodmatronic <roderickmpike@icloud.com>
2023-11-15 21:01:50 -0400

    Changed version number

--- include/config.h
+++ include/config.h
@@ -15,9 +15,9 @@ char defaulthostname[30] = "catk";
 char host_name[30] = ""; // hostname with a maximum length of 63 characters
 char username[30] = ""; // username
 
-uint32 versionnumber = "0.051";
+uint32 versionnumber = "0.052";
 char* vername = "CatK Version Boppin";
-uint32 prebootversion = "0.051";
+uint32 prebootversion = "0.052";
 char* bootargs = "n/a"; // Boot args
 
 #endif
