fc2c100d834c9a0657d88f1ca5b9b65ab4656f79
Rodmatronic <105672808+Rodmatronic@users.noreply.github.com>
2023-11-16 08:59:13 -0400

    fix to hostname.c

--- src/bin/hostname.c
+++ src/bin/hostname.c
@@ -2,10 +2,14 @@
 
 void hostname(char* input) {
     if (strlen(input) < sizeof(host_name)) {
+        current_directory = "etc";
+        rm_file(&rootfs, "hostname");
         write_to_file(&rootfs, "hostname", input);
         read_from_file(&rootfs, "hostname", buffer, sizeof(buffer));
 
         strcpy(host_name, buffer);
+
+        current_directory = "/";
         
     } else {
         printf("Hostname is too long. Maximum length is %d characters.\n", sizeof(host_name) - 1);
