58e848aff0e699618d730596ae5d9d3c02437661
Rodmatronic <roderickmpike@icloud.com>
2023-11-28 23:34:37 -0400

    Fixed kernel

--- src/kernel.c
+++ src/kernel.c
@@ -1,144 +1,74 @@
-#include "kernel.h"
 #include "console.h"
+#include "kernel.h"
+#include "libc.h"
 #include "string.h"
 #include "io_ports.h"
-#include "drivers/time.c"
-#include "drivers/memory.c"
-#include "drivers/cpu.c"
-#include "drivers/panic.c"
-#include "drivers/cdrompresent.c"
-#include "drivers/hdd.c"
-#include "drivers/syspw.c"
-#include "drivers/keyboard.c"
-#include "drivers/ethernet.c"
-#include "bin/sh.c"
-#include "bin/top.c"
-#include "bin/sleep.c"
-#include "bin/read.c"
-#include "drivers/fs.c"
-#include "config.h"
-#include "fs.h"
 #include "PreBoot.c"
-#include "drivers/crypto.c"
-#include "sbin/init.c"
-#include "sbin/termcolors.c"
-
-void printversion()
-{
-    printf("%s", vername);
-}
-
-// Function to create a new process
-int fork(void (*entry)()) {
-    if (next_pid >= MAX_PROCESSES) {
-        // Max number of processes reached
-        return -1;
-    }
-
-    int pid = next_pid++;
-    processes[pid - 1].id = pid;
-    processes[pid - 1].entry = entry;
-
-    return pid;
-}
-
-void execute_process(int pid) {
-    if (pid > 0 && pid <= next_pid) {
-        // Call the entry function for the process
-        processes[pid - 1].entry();
-        // Mark the process as terminated
-        processes[pid - 1].state = 1;
-        // Return to kernel main loop or scheduler
-    }
-}
-
-void init_processes() {
-    for (int i = 0; i < MAX_PROCESSES; i++) {
-        processes[i].id = -1;
-        processes[i].state = 0;
-    }
-}
+#include "memory.h"
+#include "cpu.h"
+#include "fs.h"
+#include "time.h"
+#include "sleep.h"
+#include "config.h"
+#include "keyboard.h"
+#include "usb.h"
+#include "sh.h"
 
 void boot() {
+    bootmessage("CatKernel boot() started");
+    printf("Boot arguments: %s\n", args);
+    bootmessage("cpuid_info(1): Attempting to get CPU info");
+    cpuid_info(1);
+    bootmessage("GetMemory(): Attempting to get Memory info");
+    GetMemory();
+    bootmessage("Showing kernel's config.h configuration \\/ ");
+    printf("   hostname: %s\n", host_name);
+    printf("   username: %s\n", username);
+    printf("   versionnumber: %s\n", versionnumber);
+    printf("   versionname: %s\n", vername);
+    printf("   arch: %s\n", arch);
+    printf("   prebootver: %s\n", prebootversion);
+    printf("   bootargs(default): %s\n", bootargs);
 
-    if (bootargs == "quiet")
-    {
-        console_init(COLOR_BLACK, COLOR_BLACK);
-    }
-
-    printf("<--BOOT-->\n");
-    printf("Created by Rodmatronics. Thank you for using CatK!\n");
-    printf("This is a BSD inspired kernel.\n");
-    printf("    It does NOT use any BSD source code\n");
     // Initialize the file table
     for (size i = 0; i < MAX_FILES; ++i) {
         rootfs.file_table[i].filename[0] = '\0';  // Empty filename indicates an unused entry
     }
-    current_directory = "/dev";
 
-    write_to_file(&rootfs, "console", "");
-    bootmessage("Getting memory...");
-    GetMemory();
-    bootmessage("Using config.catk for kernel configuration & args");
-    printf("Kernel version: %s\n", versionnumber);
-    printf("PreBoot version: %s\n", prebootversion);
-    printf("Kernel args: %s\n\n", bootargs); // Print the string
-
+    bootmessage("Perfect! &rootfs created with max files from MAX_FILES");
+    bootmessage("Creating '/' structure");
+    create_folder(&rootfs, "/bin", "/");
+    create_folder(&rootfs, "/boot", "/");
+    create_folder(&rootfs, "/cdrom", "/");
+    create_folder(&rootfs, "/dev", "/");
+    create_folder(&rootfs, "/etc", "/");
+    create_folder(&rootfs, "/home", "/");
+    create_folder(&rootfs, "/lib", "/");
+    create_folder(&rootfs, "/media", "/");
+    create_folder(&rootfs, "/mnt", "/");
+    create_folder(&rootfs, "/proc", "/");
+    create_folder(&rootfs, "/run", "/");
     create_folder(&rootfs, "/sbin", "/");
-    current_directory = "/sbin";
-    write_to_file(&rootfs, "init", "type:App\ninit");
-    current_directory = "/proc";
-    write_to_file(&rootfs, "kernel.logs", "kernel: kernlink to default init has been created!\n");
+    create_folder(&rootfs, "/sys", "/");
+    create_folder(&rootfs, "/tmp", "/");
+    create_folder(&rootfs, "/usr", "/");
+    create_folder(&rootfs, "/var", "/");
+    list_files(&rootfs, 0);
+    printf("\n");
 
-    init_processes();
+    bootmessage("Freeing system memory");
 
-    if (skipinit == 0)
-    {
-        printf_brightcyan("Run /init as init process...\n");
-        current_directory = "/proc";
-        add_data_to_file(&rootfs, "kernel.logs", "kernel: Run /init as init process...\n");
-        int found1 = execute_file;
-        current_directory = "/";
-        execute_file(&rootfs, "init");
+    size size = 6 * 1024 * 1024; // 6 MB in bytes
+    //void* memory = k_malloc(size);
+    k_malloc(size);
 
-        printf_brightcyan("Run /bin/init as init process...\n");
-        current_directory = "/proc";
-        add_data_to_file(&rootfs, "kernel.logs", "kernel: Run /bin/init as init process...\n");
-        int found2 = execute_file;
-        current_directory = "/bin";
-        execute_file(&rootfs, "init");
+    bootmessage("Allocated 6000 KB using k_malloc");
 
-        current_directory = "/proc";
-        add_data_to_file(&rootfs, "kernel.logs", "kernel: init is being started\n");
-        printf_brightcyan("Run /sbin/init as init process...\n");
-        current_directory = "/proc";
-        add_data_to_file(&rootfs, "kernel.logs", "kernel: Run /sbin/init as init process...\n");
-        current_directory = "/sbin";
-        int found3 = execute_file;
-        execute_file(&rootfs, "init");
+    bootmessage("Keyboard init ");
+    init_keyboard();
 
-        //If this happens, something is monumentally fucked up!
-        if (found3 != 0) {
-            
-            for (int i = 5; i > 0;)
-            {
-                add_data_to_file(&rootfs, "kernel.logs", "kernel: init is being started\n");
-                printf("INIT failed! Retrying...\n");
-                sleep(1);
-                current_directory = "/sbin";
-                execute_file(&rootfs, "init");
-                i--;
-            }
+    k_sh();
 
-            panic("Something is very wrong with INIT! Failed to boot!");
-
-        }
-    }
-
-    if (skipinit == 1)
-    {
-        sh();
-    }
 }
 
 void kmain() {
@@ -150,27 +80,8 @@ void bootmessage(const char* str) { // Use const char* for the string parameter
     printf("kernel: %s\n", str); // Print the message and the string
 }
 
+/*
 void catkmessagefixed(int NUM) { // Use const char* for the string parameter
-    if (NUM == 1)
-    {
-        console_gotoxy(50, rows);
-        printf_darkcyan("[ "); // Print the message and the string
-        printf_brightcyan("OK"); // Print the message and the string
-        printf_darkcyan(" ]"); // Print the message and the string
-    }else
-    if (NUM == 2)
-    {
-        console_gotoxy(50, rows);
-        printf_darkcyan("["); // Print the message and the string
-        printf_darkcyan("WARN");
-        printf_darkcyan("]");
-    }else
-    if (NUM == 3)
-    {
-        console_gotoxy(50, rows);
-        printf_darkcyan("["); // Print the message and the string
-        printf_red("ERR!");
-        printf_darkcyan("]");
-    }
-    rows++;
-}
+   
+    //do nothing
+}*/
