64803622aaafa6f510470fb0b045dac77d014573
Rodmatronics <105672808+Rodmatronic@users.noreply.github.com>
2024-02-09 09:43:42 -0400

    Changed read-only system version

--- src/kernel/include/config.h
+++ src/kernel/include/config.h
@@ -3,7 +3,7 @@
 static char * sys_arch = "X86/intel";
 static char * sys_term = "80x25";
 static char * sys_sesh = "/bin/k_sh";
-static char * sys_ver = "0.07";
+static char * sys_ver = "0.072";
 static char * sys_home = "/home/root";
 static char * sys_name = "Catkernel";
 static char * sys_user = "root";
